1b0677142e909a0491fa1c914db2a5427a108665
andrepolischuk <andre.polischuk@gmail.com>
2015-04-16 17:41:14 +0300

    Refactor

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * Module dependencies
  */
 
+var each = require('ea');
 var tpl = require('mnml-tpl');
 
 try {
@@ -14,7 +15,30 @@ try {
 }
 
 var conversions = require('./lib/conversions');
-var models = require('./lib/models');
+
+/**
+ * Color matchers
+ */
+
+var matchers = {};
+matchers.keyword = /^([A-Za-z]+)$/;
+matchers.hex = /^#([a-f0-9]{3}([a-f0-9]{3})?)$/i;
+matchers.rgb = /^rgba?\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,?\s*([\d\.]+)?\s*\)$/;
+matchers.cmyk = /^cmyk\(\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\)$/;
+matchers.hsl = /^hsla?\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,?\s*([\d\.]+)?\s*\)?$/;
+matchers.hsv = /^hsva?\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,?\s*([\d\.]+)?\s*\)?$/;
+
+/**
+ * Color format
+ */
+
+var format = {};
+format.keyword = [':k'];
+format.hex = ['#:k'];
+format.rgb = ['rgb(:r, :g, :b)', 'rgba(:r, :g, :b, :a)'];
+format.cmyk = ['cmyk(:c%, :m%, :y%, :k%)'];
+format.hsl = ['hsl(:h, :s%, :l%)', 'hsla(:h, :s%, :l%, :a)'];
+format.hsv = ['hsv(:h, :s%, :v%)', 'hsva(:h, :s%, :v%, :a)'];
 
 /**
  * Expose parser
@@ -24,84 +48,52 @@ var models = require('./lib/models');
  * @api public
  */
 
-module.exports = function(color, parser, matcher) {
-  for (var mod in models) {
-    if (models.hasOwnProperty(mod)) {
-      parser = routeIn(mod);
-      matcher = models[mod].matcher;
+module.exports = function(color) {
+  var model;
 
-      switch (true) {
-        case matcher.test(color):
-          return parser.apply(null, matcher.exec(color).splice(1));
-        case type(color) === 'object' && mod.charAt(mod.length - 1) in color:
-          return parser.apply(null, toArray(color));
-      }
-    }
-  }
+  each(matchers, function(matcher, m) {
+    var key = /(hex|keyword)/.test(m) ? m : m.charAt(2);
+    if (type(color) === 'string' && matcher.test(color)) model = m;
+    if (type(color) === 'object' && key in color) model = m;
+  });
+
+  if (!model) return;
+
+  var matcher = matchers[model];
+  if (type(color) === 'object') return parse(model, toArray(color));
+  return parse(model, matcher.exec(color).splice(1));
 };
 
 /**
- * Expose models parse
+ * Expose parser via model
+ *
+ * @param  {Array} val
+ * @param  {Arguments}
+ * @return {Object}
  * @api public
  */
 
-for (var m in models) {
-  if (models.hasOwnProperty(m)) {
-    module.exports[m] = routeIn(m);
-
-    if (/(hex|keyword)/.test(m)) {
-      Color.prototype[m] = routeOut(m, 'str');
-    } else {
-      Color.prototype[m] = routeOut(m, 'obj');
-      Color.prototype[m + 'Array'] = routeOut(m, 'arr');
-      Color.prototype[m + 'String'] = routeOut(m, 'str');
-    }
-  }
-}
+each(matchers, function(matcher, model) {
+  module.exports[model] = function(val) {
+    if (type(val) === 'array') return parse(model, val);
+    return parse.apply(null, [model].concat([].splice.call(arguments, 0)));
+  };
+});
 
 /**
- * Input router
+ * Parse color
  *
  * @param  {String} model
- * @param  {Array} arg
+ * @param  {Array} val
+ * @param  {Arguments}
  * @return {Object}
  * @api private
  */
 
-function routeIn(model) {
-  return function(arg) {
-    if (type(arg) === 'array') return routeIn(model).apply(null, arg);
-    if (model === 'rgb') return new Color(arguments);
-    return new Color(conversions[model + '2rgb'].apply(null, arguments));
-  };
-}
-
-/**
- * Output router
- *
- * @param  {String} model
- * @param  {String} output
- * @return {Object|String|Array}
- * @api private
- */
-
-function routeOut(model, output) {
-  return function() {
-    var obj = this.vals;
-    var arr = toArray(obj);
-
-    if (model !== 'rgb') {
-      arr = conversions['rgb2' + model].apply(null, arr);
-      obj = toObject(models[model].keys, arr);
-    }
-
-    if (arr.length > 3 && !isDefined(arr[3])) {
-      arr.pop();
-    }
-
-    if (output === 'str') return tpl(models[model].format(arr))(obj);
-    return output === 'arr' ? arr : obj;
-  };
+function parse(model, val) {
+  if (type(val) === 'array') return parse.apply(null, [model].concat(val));
+  var args = [].splice.call(arguments, 1);
+  return new Color(conversions[model + '2rgb'].apply(null, args));
 }
 
 /**
@@ -115,11 +107,9 @@ function routeOut(model, output) {
 
 function toObject(keys, arr) {
   var obj = {};
-  for (var i = 0; i < keys.length; i++) {
-    if (isDefined(arr[i])) {
-      obj[type(keys) === 'string' ? keys.charAt(i) : keys[i]] = arr[i];
-    }
-  }
+  each(keys, function(char, i) {
+    if (isDefined(arr[i])) obj[char] = arr[i];
+  });
   return obj;
 }
 
@@ -133,11 +123,9 @@ function toObject(keys, arr) {
 
 function toArray(obj) {
   var arr = [];
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key) && isDefined(obj[key])) {
-      arr.push(obj[key]);
-    }
-  }
+  each(obj, function(val) {
+    if (isDefined(val)) arr.push(val);
+  });
   return arr;
 }
 
@@ -162,13 +150,85 @@ function isDefined(val) {
 
 function Color(color) {
   this.vals = toObject('rgba', color);
-
-  for (var c in this.vals) {
-    if (this.vals.hasOwnProperty(c)) {
-      this.vals[c] = +this.vals[c];
-    }
-  }
 }
+
+/**
+ * Return converted color array
+ *
+ * @param  {String} model
+ * @return {Array}
+ * @api public
+ */
+
+Color.prototype.getArray = function(model) {
+  var arr = conversions['rgb2' + model].apply(null, toArray(this.vals));
+  if (arr.length > 3 && !isDefined(arr[3])) arr.pop();
+  return arr;
+};
+
+/**
+ * Return converted color object
+ *
+ * @param  {String} model
+ * @return {Object}
+ * @api public
+ */
+
+Color.prototype.getObject = function(model) {
+  var arr = this.getArray(model);
+  var keys = /(keyword|hex)/.test(model) ? 'k' : model;
+  if (arr.length > keys.length) keys += 'a';
+  return toObject(keys, arr);
+};
+
+/**
+ * Return converted color string
+ *
+ * @param  {String} model
+ * @return {String}
+ * @api public
+ */
+
+Color.prototype.getString = function(model) {
+  var obj = this.getObject(model);
+  var alpha = 'a' in obj;
+  var fmt = format[model][alpha ? 1 : 0];
+  return tpl(fmt)(obj);
+};
+
+/**
+ * Return converted to HEX and Keyword
+ *
+ * @return {String}
+ * @api public
+ */
+
+each(['keyword', 'hex'], function(model) {
+  Color.prototype[model] = function() {
+    return this.getString(model);
+  };
+});
+
+/**
+ * Return converted to RGB, CMYK, HSL and HSV
+ *
+ * @return {Mixed}
+ * @api public
+ */
+
+each(['rgb', 'cmyk', 'hsl', 'hsv'], function(model) {
+  Color.prototype[model] = function() {
+    return this.getObject(model);
+  };
+
+  Color.prototype[model + 'Array'] = function() {
+    return this.getArray(model);
+  };
+
+  Color.prototype[model + 'String'] = function() {
+    return this.getString(model);
+  };
+});
 
 /**
  * To grayscale
@@ -282,7 +342,6 @@ Color.prototype.saturate = function(val) {
   return this;
 };
 
-
 /**
  * Desaturate color
  *
@@ -298,127 +357,103 @@ Color.prototype.desaturate = function(val) {
 };
 
 /**
- * Return or set red component
- * @api public
- */
-
-Color.prototype.red = rgbaComponents('r');
-
-/**
- * Return or set green component
- * @api public
- */
-
-Color.prototype.green = rgbaComponents('g');
-
-/**
- * Return or set blue component
- * @api public
- */
-
-Color.prototype.blue = rgbaComponents('b');
-
-/**
- * Return or set alpha component
- * @api public
- */
-
-Color.prototype.alpha = rgbaComponents('a');
-
-/**
- * Return or set cyan component
- * @api public
- */
-
-Color.prototype.cyan = colorComponents('cmyk', 0);
-
-/**
- * Return or set magenta component
- * @api public
- */
-
-Color.prototype.magenta = colorComponents('cmyk', 1);
-
-/**
- * Return or set yellow component
- * @api public
- */
-
-Color.prototype.yellow = colorComponents('cmyk', 2);
-
-/**
- * Return or set key component
- * @api public
- */
-
-Color.prototype.key = colorComponents('cmyk', 3);
-
-/**
- * Return or set hue component
- * @api public
- */
-
-Color.prototype.hue = colorComponents('hsl', 0);
-
-/**
- * Return or set saturation component
- * @api public
- */
-
-Color.prototype.saturation = colorComponents('hsl', 1);
-
-/**
- * Return or set lightness component
- * @api public
- */
-
-Color.prototype.lightness = colorComponents('hsl', 2);
-
-/**
- * Return or set value component
- * @api public
- */
-
-Color.prototype.value = colorComponents('hsv', 2);
-
-/**
- * Return or set RGBA components
+ * Return or set RGB components
  *
- * @param  {String} prop
  * @param  {Number} val
  * @return {Number|Object}
- * @api private
+ * @api public
  */
 
-function rgbaComponents(prop) {
-  return function(val) {
-    if (type(val) !== 'number') return this.vals[prop];
-    this.vals[prop] = val;
-    return this;
+each(['red', 'green', 'blue', 'alpha'], function(prop) {
+  Color.prototype[prop] = function(val) {
+    return this.component('rgb', prop, val);
   };
-}
+});
 
 /**
- * Return or set CMYK, HSLA and HSVA components
+ * Return or set CMYK components
+ *
+ * @param  {Number} val
+ * @return {Number|Object}
+ * @api public
+ */
+
+each(['cyan', 'magenta', 'yellow', 'key'], function(prop) {
+  Color.prototype[prop] = function(val) {
+    return this.component('cmyk', prop, val);
+  };
+});
+
+/**
+ * Return or set HSL components
+ *
+ * @param  {Number} val
+ * @return {Number|Object}
+ * @api public
+ */
+
+each(['hue', 'saturation', 'lightness'], function(prop) {
+  Color.prototype[prop] = function(val) {
+    return this.component('hsl', prop, val);
+  };
+});
+
+/**
+ * Return or set HSV components
+ *
+ * @param  {Number} val
+ * @return {Number|Object}
+ * @api public
+ */
+
+Color.prototype.value = function(val) {
+  return this.component('hsv', 'value', val);
+};
+
+/**
+ * Return or set component
  *
  * @param  {String} model
  * @param  {String} prop
  * @param  {Number} val
  * @return {Number|Object}
- * @api private
+ * @api public
  */
 
-function colorComponents(model, prop) {
-  return function(val) {
-    var color = conversions['rgb2' + model].apply(null, toArray(this.vals));
-    if (type(val) !== 'number') return color[prop];
-    color[prop] = val;
+Color.prototype.component = function(model, prop, val) {
+  if (type(val) !== 'number') return this.getComponent(model, prop);
+  return this.setComponent(model, prop, val);
+};
 
-    this.vals = toObject(
-      'rgba',
-      conversions[model + '2rgb'].apply(null, color)
-    );
+/**
+ * Return component
+ *
+ * @param  {String} model
+ * @param  {String} prop
+ * @return {Number}
+ * @api public
+ */
 
-    return this;
-  };
-}
+Color.prototype.getComponent = function(model, prop) {
+  var color = conversions['rgb2' + model].apply(null, toArray(this.vals));
+  var index = prop === 'alpha' ? 3 : model.indexOf(prop.charAt(0));
+  return color[index];
+};
+
+/**
+ * Set component
+ *
+ * @param  {String} model
+ * @param  {String} prop
+ * @param  {Number} val
+ * @return {Object}
+ * @api public
+ */
+
+Color.prototype.setComponent = function(model, prop, val) {
+  var color = conversions['rgb2' + model].apply(null, toArray(this.vals));
+  var index = prop === 'alpha' ? 3 : model.indexOf(prop.charAt(0));
+  color[index] = val;
+  this.vals = toObject('rgba', conversions[model + '2rgb'].apply(null, color));
+  return this;
+};
